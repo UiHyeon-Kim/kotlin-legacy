82991dcb78ba0f1679a76f7e0540e52f939dd50f
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-02 23:03:20 +0900

    refactor: 하드코딩 상수화

--- src/main/kotlin/legacy/feature/view/CustomerView.kt
+++ src/main/kotlin/legacy/feature/view/CustomerView.kt
@@ -6,29 +6,29 @@ import legacy.feature.contract.CustomerContract
 
 class CustomerView : CustomerContract.View {
     override fun printSystemExecutionMessage() {
-        println("===== 고객 관리 시스템 실행 =====\n")
+        println(SYSTEM_EXECUTION_MESSAGE)
     }
 
     override fun printCustomerAddMessage(id: String, name: String, status: CustomerState) {
-        print("[INFO] 고객 등록 성공: ")
+        print(REGISTRATION_SUCCESS)
         printCustomer(id, name, status)
     }
 
     override fun printCustomerUpdateMessage(id: String, oldName: String, newName: String) {
         val changeName = "$oldName -> $newName"
-        print("[INFO] 고객 수정 성공: ")
+        print(MODIFICATION_SUCCESS)
         printCustomer(id, changeName)
     }
 
     override fun printCustomerDeleteMessage(id: String, name: String) {
-        print("[INFO] 고객 삭제 성공: ")
+        print(DELETION_SUCCESS)
         printCustomer(id, name)
     }
 
     override fun printCustomerInquiryMessage(customers: List<Customer>) {
         val customerCount = customers.size
 
-        println("\n----- 고객 조회 결과 -----")
+        println(INQUIRY_RESULT_MESSAGE)
         println("총 고객 수: $customerCount")
         for ((index, customer) in customers.withIndex()) {
             print("${index + 1}. ")
@@ -42,5 +42,16 @@ class CustomerView : CustomerContract.View {
             null -> println("ID: $id | 이름: $name")
             else -> println("ID: $id | 이름: $name | 상태: $status")
         }
+    }
+
+    companion object {
+        private const val INFO = "[INFO]"
+
+        private const val SYSTEM_EXECUTION_MESSAGE = "===== 고객 관리 시스템 실행 =====\n"
+        private const val INQUIRY_RESULT_MESSAGE = "\n----- 고객 조회 결과 -----"
+
+        private const val REGISTRATION_SUCCESS = "$INFO 고객 등록 성공: "
+        private const val MODIFICATION_SUCCESS = "$INFO 고객 수정 성공: "
+        private const val DELETION_SUCCESS = "$INFO 고객 삭제 성공: "
     }
 }
