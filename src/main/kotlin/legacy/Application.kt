e587db9018dc7627468dc3b0f139420dc10b4821
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-02 21:06:22 +0900

    feat: MVP 구조 의존성 설정

--- src/main/kotlin/legacy/Application.kt
+++ src/main/kotlin/legacy/Application.kt
@@ -1,8 +1,14 @@
 package legacy
 
+import legacy.data.repository.CustomerRepository
+import legacy.data.repository.CustomerRepositoryImpl
+import legacy.feature.contract.CustomerContract
 import legacy.feature.presenter.CustomerPresenter
+import legacy.feature.view.CustomerView
 
 fun main() {
-    val customerPresenter = CustomerPresenter()
+    val customerView: CustomerContract.View = CustomerView()
+    val repository: CustomerRepository = CustomerRepositoryImpl()
+    val customerPresenter: CustomerContract.Presenter = CustomerPresenter(customerView, repository)
     customerPresenter.execute()
 }
