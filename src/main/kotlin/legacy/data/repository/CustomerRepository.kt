fd64d58bebb1d0aea7f11b036fc0595c9b93963c
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-02 20:55:50 +0900

    feat: repository 인터페이스 매개변수 변경

--- src/main/kotlin/legacy/data/repository/CustomerRepository.kt
+++ src/main/kotlin/legacy/data/repository/CustomerRepository.kt
@@ -1,10 +1,12 @@
 package legacy.data.repository
 
+import legacy.data.model.Customer
 import legacy.data.model.CustomerState
 
 interface CustomerRepository {
     fun insertCustomer(id: String, name: String, status: CustomerState)
-    fun updateCustomer(id: String, newName: String)
-    fun deleteCustomer(id: String)
-    fun selectCustomer()
+    fun updateCustomer(id: String, name: String)
+    fun deleteCustomer(customer: Customer)
+    fun selectCustomer(id: String): Customer
+    fun getCustomers(): List<Customer>
 }
