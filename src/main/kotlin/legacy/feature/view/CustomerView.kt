1e9c484dca40fa0848f334ffe5f2bedca58f87b0
김의현 <125240447+UiHyeon-Kim@users.noreply.github.com>
2025-05-02 20:56:42 +0900

    feat: 고객 추가, 수정, 삭제 함수 구현

--- src/main/kotlin/legacy/feature/view/CustomerView.kt
+++ src/main/kotlin/legacy/feature/view/CustomerView.kt
@@ -1,11 +1,28 @@
 package legacy.feature.view
 
 import legacy.data.model.Customer
+import legacy.data.model.CustomerState
 import legacy.feature.contract.CustomerContract
 
 class CustomerView : CustomerContract.View {
     override fun printSystemExecutionMessage() {
         println("===== 고객 관리 시스템 실행 =====\n")
+    }
+
+    override fun printCustomerAddMessage(id: String, name: String, status: CustomerState) {
+        print("[INFO] 고객 등록 성공: ")
+        printCustomer(id, name, status)
+    }
+
+    override fun printCustomerUpdateMessage(id: String, oldName: String, newName: String) {
+        val changeName = "$oldName -> $newName"
+        print("[INFO] 고객 수정 성공: ")
+        printCustomer(id, changeName)
+    }
+
+    override fun printCustomerDeleteMessage(id: String, name: String) {
+        print("[INFO] 고객 삭제 성공: ")
+        printCustomer(id, name)
     }
 
     override fun printCustomerInquiryMessage(customers: List<Customer>) {
